84a42000d698ccc72806b1d38ebbe96f05de3934
Amiiiiiiinn <117063224+Amiiiiiiinn@users.noreply.github.com>
2024-09-11 01:23:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,3 +51,15 @@ resource "aws_dynamodb_table" "terraform_locks" {
     Environment = "Production"
   }
 }
+#################################################
+# Terraform backend configuration
+#################################################
+terraform {
+  backend "s3" {
+    bucket         = aws_s3_bucket.mybucket.bucket
+    key            = "terraform/state"   # Path to store the state file in S3
+    region         = "us-east-1"
+    dynamodb_table = aws_dynamodb_table.terraform_locks.name
+    encrypt        = true                # Server-side encryption for the state file
+  }
+}
