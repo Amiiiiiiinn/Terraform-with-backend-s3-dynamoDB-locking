ff83feeef545f3fa9cfde90d7b5b34a2522f762f
Amiiiiiiinn <117063224+Amiiiiiiinn@users.noreply.github.com>
2024-09-11 02:33:07 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
 backend "s3" {
-        bucket ="statebackend55443amin"
+        bucket ="statebackend55443ameen"
         dynamodb_table = "terraform-lock-table"
         key = "global/mystatefile/terraform.tf"
         region = "us-east-1"
