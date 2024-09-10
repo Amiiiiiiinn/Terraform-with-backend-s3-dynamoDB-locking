fa5f98428031d6e2af6545ee06d4104505781b80
Amiiiiiiinn <117063224+Amiiiiiiinn@users.noreply.github.com>
2024-09-11 01:02:19 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "amin260_bucket"
+
+  tags = {
+    Name        = "MyS3Bucket"
+    Environment = "Production"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.mybucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
