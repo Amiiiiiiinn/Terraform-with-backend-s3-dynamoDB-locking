540546eb2159da1b800a50ff58916d34f48ec43b
Amiiiiiiinn <117063224+Amiiiiiiinn@users.noreply.github.com>
2024-09-11 01:06:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,13 +10,18 @@ resource "aws_s3_bucket" "mybucket" {
     Environment = "Production"
   }
 }
-
+################################################# 
+# Enable bucket versioning 
+################################################# 
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.mybucket.id
   versioning_configuration {
     status = "Enabled"
   }
 }
+################################################# 
+# S3 bucket server side encryption
+################################################# 
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   bucket = aws_s3_bucket.mybucket.id
@@ -25,5 +30,24 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
+  }
+}
+
+################################################# 
+# Create a DynamoDB table for state locking
+################################################# 
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"  # DynamoDB auto-scales based on usage
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "TerraformLockTable"
+    Environment = "Production"
   }
 }
